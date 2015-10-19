9126378360beb4eada616c1c8a5a17a93b282518
yuweipei <yuweipei@seas.upenn.edu>
2015-10-19 14:58:29 -0400

    Final

---
+++ app/src/main/java/com/example/david/myappportfolio/MainActivityFragment.java
@@ -0,0 +1,27 @@
+package com.example.david.myappportfolio;
+
+import android.content.Context;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+
+}
